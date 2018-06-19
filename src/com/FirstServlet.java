40afd5e2a1107a7d0f79b1cd18df6fab80421790
Andrea Lakoseljac <alakoseljac@croz.net>
2018-06-19 17:05:01 +0200

    Client ask me to change a comment
    The client asked for the comment to be added. I made that change for
    them.
    
    * Client asked for comment change
    * i don't see this effecting anything
    
    Closes_Bug: #129161

---
+++ src/com/FirstServlet.java
@@ -0,0 +1,59 @@
+package com;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+
+    //Random Comments
+
+    public FirstServlet(){
+        super();
+        System.out.println("Inside the no-arg constructor():::::");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("fname");
+        String lastName = req.getParameter("lname");
+        String fullName = firstName + lastName;
+        System.out.println("My name is " + fullName);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("fname");
+        String lastName = req.getParameter("lname");
+        String fullName = firstName + lastName;
+        //PrintWriter writer = resp.getWriter();
+        //writer.println("My name is " + fullName);
+        //writer.println("<html><body><h1>My name is "+fullName+"</h1></body></html>");
+        //resp.sendRedirect("test.html");
+
+        req.setAttribute("name", fullName);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/output.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("Inside the init()::::::");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("Inside the service():::::");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("Inside the destroy():::::");
+    }
+}
